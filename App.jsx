171b8b25a74c8aa222717eb79cf7f0ce6762281a
wdq <284138410@qq.com>
2017-09-20 16:38:01 +0800

    增加路由

--- App.jsx
+++ App.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
 
 class App extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         header: "Header from state...",
+         content: "Content from state..."
+      }
+   }
    render() {
    var i = 1;
       return (
          <div>
+            {/*
             <p>Welcome to my react program demo !</p>
             <p>{i == 1 ? 'HAHA' : 'HEiHEI'}</p>
-            {/*Multi line comment...*/}
+            */}
+            <Header/>
+            <Content/>
          </div>
       )
    }
 }
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Header</h1>
+         </div>
+      );
+   }
+}
+
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2>Content</h2>
+            <p>The content text!!!</p>
+         </div>
+      );
+   }
+}
+
 
 export default App;
--- main.js
+++ main.js
@@ -1,5 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import Home from './App.jsx';
+import Table from './Table.jsx';
+import Form from './Form.jsx';
+import { BrowserRouter,Route,NavLink,Switch  } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const isActiveFunc = (match, location) => {
+  console.log(match,location)
+}
+// NavBar
+const NavBar = () => (
+        <nav>
+           <NavLink activeStyle={{color: 'green'}} exact activeClassName="active" to="/">Home</NavLink>
+           <NavLink activeStyle={{color: 'green'}} to={{pathname: '/table',state:{id:'1'}}}>Table</NavLink>
+           <NavLink isActive={isActiveFunc} activeClassName="active" to="/form">Form</NavLink>
+        </nav>
+)
+
+const RouteTest = () => (
+      <BrowserRouter basename="/">
+        <div>
+              <NavBar />
+              <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/table" component={Table}/>
+                <Route path="/form" component={Form}/>
+              </Switch>
+        </div>
+      </BrowserRouter>
+)
+
+ReactDOM.render((
+   <RouteTest />
+), document.getElementById('routerTest'))
+
+// ReactDOM.render(<AppIndex />, document.getElementById('app'));
+// ReactDOM.render(<Table />, document.getElementById('table'));
+// ReactDOM.render(<Form />, document.getElementById('form'));
